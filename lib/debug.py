938b962e8273a5a3cd09e157441961b3eb0b7e9c
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-11 19:35:41 +0300

    Completed lab

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,8 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+    
+from lib import CONN, CURSOR
+from lib.song import Song
+
+import pytest; pytest.set_trace()
